1e1e6cd0713a9c884878ff14276b651454969cfa
mima <kukuri3@gmail.com>
2015-09-08 11:40:24 +0900

    first commit

---
+++ get_commandline_arg/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cmds = System.Environment.GetCommandLineArgs();
+            //コマンドライン引数を列挙する
+            Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+            StreamWriter writer =  new StreamWriter("arg.txt", true, sjisEnc);
+
+            foreach (string cmd in cmds)
+            {
+                Console.WriteLine(cmd);
+                writer.WriteLine(cmd);
+            }
+            writer.Close();
+
+        }
+    }
+}
